33af324cfa1b23d29247af794f397e3303630599
coskunaydgn <coskun.aydgn@gmail.com>
2024-02-07 09:41:10 +0100

    ny

---
+++ src/main/java/org/example/moduler/ModulerApplication.java
@@ -0,0 +1,13 @@
+package org.example.moduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/moduler/ModulerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.moduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
